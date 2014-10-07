f65cc2e7a1a202bd55368e1e9c9f6999a11192f1
deebuls <deebuls@gmail.com>
2014-10-07 11:08:48 +0200

    initial comit

---
+++ build/CMakeFiles/youbot_kdl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/youbot_kdl.dir/src/main.cpp.o"
+  "../bin/youbot_kdl.pdb"
+  "../bin/youbot_kdl"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/youbot_kdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
