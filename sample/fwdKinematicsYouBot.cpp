ebbbe547b613010bdce31d77d617af2e1fcce007
deebuls <deebuls@gmail.com>
2014-12-10 12:24:33 +0100

    Updated DH parameters . Not tested on Hardware

--- sample/fwdKinematicsYouBot.cpp
+++ sample/fwdKinematicsYouBot.cpp
@@ -31,33 +31,40 @@ int main( int argc, char** argv )
     KDL::Frame frame_5;
 
     KDL::Frame end_factor;
+    double offset[5] = {
+        (-169 * M_PI/180),
+        (-65 * M_PI/180),
+        (151 * M_PI/180),
+        (-102.5 * M_PI/180),
+        (-165 * M_PI/180)
+    };
     
     //Base frame 
     arm_base_frame = KDL::Frame::DH(0.0, M_PI, 0.147, 0.0);
 
     //Frame 1 and Joint 1
     joint_1 = KDL::Joint(KDL::Joint::RotZ);
-    frame_1 = KDL::Frame::DH(0.033, +M_PI_2, 0.000, 0);
+    frame_1 = KDL::Frame::DH(0.033, +M_PI_2, 0.000, offset[0]);
 
     //Frame 2 and Joint 2
     joint_2 = KDL::Joint( KDL::Joint::RotZ);
-    frame_2 = KDL::Frame::DH(0.155, 0, 0.000, 0);
+    frame_2 = KDL::Frame::DH(0.155, 0, 0.000, offset[1]-M_PI_2);
 
     //Frame 3 and Joint 3
     joint_3 = KDL::Joint(KDL::Joint::RotZ);
-    frame_3 =  KDL::Frame::DH(0.135, 0, 0.000, 0);
+    frame_3 =  KDL::Frame::DH(0.135, 0, 0.000, offset[2]+ 0);
 
     //Frame 4 and Joint 4
     joint_4 = KDL::Joint(KDL::Joint::RotZ);
-    frame_4 =  KDL::Frame::DH(0.0, 0, 0.000,0 );
+    frame_4 =  KDL::Frame::DH(0.0, 0, 0.000,offset[3]+ M_PI_2 );
 
     //Frame 5 and Joint 5
     joint_5 = KDL::Joint(KDL::Joint::RotZ);
-    frame_5 =  KDL::Frame::DH(0.0, -M_PI_2, 0.000, 0);
+    frame_5 =  KDL::Frame::DH(0.0, -M_PI_2, 0.000, offset[4]+ 0);
 
 
     //Frame 6
-    end_factor = KDL::Frame::DH(0.0, 0, -0.218, 0);
+    end_factor = KDL::Frame::DH(0.0, +M_PI, -0.218,  0);
 
     //Adding the chain
     chain.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::None), arm_base_frame));
@@ -78,23 +85,42 @@ int main( int argc, char** argv )
  
     // Assign some values to the joint positions
     //Assigning to candle position
-	 jointpositions(0) = (double) 2.96244 ;
-	 jointpositions(1) = (double)1.04883 ;
-	 jointpositions(2) = (double)-2.43523;
-	 jointpositions(3) = (double)1.73184 ;
-	 jointpositions(4) = (double)2.91062;
     
-    
+	 jointpositions(0) = -offset[0] ;
+	 jointpositions(1) = -offset[1];
+	 jointpositions(2) = -offset[2];
+	 jointpositions(3) = - offset[3];
+	 jointpositions(4) = -offset[4];
+    /*
+	 jointpositions(0) = (double)0 ;
+	 jointpositions(1) = (double)0;
+	 jointpositions(2) = (double)0;
+	 jointpositions(3) = (double)0;
+	 jointpositions(4) = (double)0;
+   */ 
     // Create the frame that will contain the resul
-KDL::Frame cartpos;    
+    KDL::Frame cartpos;    
  
     // Calculate forward position kinematics
     bool kinematics_status;
-    kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
+    for(int i =0 ; i<8 ; i++)
+    {
+    kinematics_status = fksolver.JntToCart(jointpositions,cartpos, i);
     if(kinematics_status>=0){
+        std::cout << cartpos.M<<std::endl;
         std::cout << cartpos.p <<std::endl;
-        printf("%s \n","Succes, thanks KDL!");
+        printf("Joint %i \n", i);
     }else{
         printf("%s \n","Error: could not calculate forward kinematics :(");
     }
+    }
+    kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
+    if(kinematics_status>=0){
+        std::cout << cartpos.M<<std::endl;
+        std::cout << cartpos.p <<std::endl;
+        printf("End Joint  \n" );
+    }else{
+        printf("%s \n","Error: could not calculate forward kinematics :(");
+    }
+
 }
